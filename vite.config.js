d5630b6bdac2fc07c77380ce431b918db863f6d6
vyomgoyal <vyom.is22@bmsce.ac.in>
2025-01-22 12:23:59 +0530

    bhai deploy ho jaa4

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,10 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['@react-three/fiber', '@react-three/drei'],
+      external: ['@react-three/fiber', '@react-three/drei', 'react-responsive'],
     },
   },
   optimizeDeps: {
-    include: ['@react-three/fiber', '@react-three/drei'],
+    include: ['@react-three/fiber', '@react-three/drei', 'react-responsive'],
   },
 });
