454f377c9044b96307e46c3f5c67d3a55617e919
vyomgoyal <vyom.is22@bmsce.ac.in>
2025-01-22 13:26:15 +0530

    initial code

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['@react-three/fiber', '@react-three/drei', 'react-responsive'],
-    },
-  },
-  optimizeDeps: {
-    include: ['@react-three/fiber', '@react-three/drei', 'react-responsive'], //comment
-  },
+
 });
